756b60b3d42c10c86821ceeeaf566f881615e9a1
choikyungsoo <ryan1954@naver.com>
2025-07-24 16:41:59 +0900

    feat: 공통 컴포넌트 Drawers.tsx 구현

---
+++ src/components/common/Drawers.tsx
@@ -0,0 +1,36 @@
+import {type PropsWithChildren, type ReactNode} from "react";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent, DrawerFooter,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+import CloseIcon from "@/components/icons/system/CloseIcon.tsx";
+
+interface DrawerProps {
+    trigger : ReactNode
+}
+
+const Drawers = ({trigger, children}: PropsWithChildren<DrawerProps>) => {
+    return (
+        <Drawer>
+            <DrawerTrigger asChild>
+                {trigger}
+            </DrawerTrigger>
+            <DrawerContent className="fixed bottom-4 inset-x-0 mx-auto max-w-[360px] w-full [&>div:first-child]:hidden rounded-t-030 !rounded-t-030 overflow-visible">
+                <div className="pl-024 pr-024">
+                    <DrawerFooter className="w-full flex items-end p-0">
+                        <DrawerClose asChild>
+                            <button className="p-012 pr-0 mt-[8px]">
+                                <CloseIcon/>
+                            </button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                    {children}
+                </div>
+            </DrawerContent>
+        </Drawer>
+    );
+};
+
+export default Drawers;
