52c3c2dbcb6858412917bacfe7c72983e57508bb
unknown <jjjk0605@naver.com>
2025-07-25 01:02:54 +0900

    feat: 메인 페이지 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,28 @@
-import { useEffect } from "react";
+import AppBar from "@/components/common/AppBar";
+import Headline from "@/components/common/Headline";
+import Navigation from "@/components/common/Navigation";
+import UrlInput from "@/components/pages/Home/UrlInput";
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    document.documentElement.lang = i18n.language;
-  }, [i18n.language]);
+  const { t } = useTranslation(["appBar", "headline"]);
 
   return (
-    <div>
-      <h1 className="font-family-brand">{t("welcome")}</h1>
-      <p>{t("description")}</p>
-      <button onClick={() => i18n.changeLanguage("en")}>영어로</button>
-      <button onClick={() => i18n.changeLanguage("ko")}>한국어로</button>
+    <div className="relative w-[360px] h-dvh bg-[url('/bg.png')] bg-center">
+      <AppBar title={t("appBar_navi-01")} />
+
+      <div className="mt-[155px] flex flex-col items-center gap-[35px]">
+        <Headline
+          title={t("headline:headline_title_home")}
+          className="w-[250px]"
+          description={t("headline:headline_supportingText_home")}
+          descriptionClassName="text-sy_label-normal"
+        />
+
+        <UrlInput />
+      </div>
+
+      <Navigation className="fixed bottom-0" />
     </div>
   );
 };
