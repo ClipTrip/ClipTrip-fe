67a7538e9f42824bb34536fa332a48d5bc554c1d
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-07-25 17:42:43 +0900

    [Feature/issue-016] 공통컴포넌트 개발 (#26)
    * feat: 공통 컴포넌트 Drawers.tsx 구현
    
    * chore: Drawers 누락 파일 커밋
    
    구현완료
    
    * feat: 공통 컴포넌트 SelectField.tsx 구현
    
    * feat: 공통 컴포넌트 CheckBox.tsx 개발
    
    * refactor: SelectField 선택값을 외부에서 받을수 있도록 변경
    
    * refactor: CheckBox isActive에 ?로 없을 경우를 생각하여 처리
    
    * fix: Drawer에서 DialogTilte이 없어 오류나는거 수정
    
    * refactor: 코드 리뷰에 따른리팩토링 - Drawers
    
    * fix: 외부에서 체크값을 받을 수있도록 checkbox컴포넌트 수정
    
    * refactor: 코드리뷰에 따라 SelectField 옵셔널 처리

---
+++ src/components/common/Drawers.tsx
@@ -0,0 +1,39 @@
+import {type PropsWithChildren, type ReactNode} from "react";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerContent,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+import CloseIcon from "@/components/icons/system/CloseIcon.tsx";
+
+interface DrawerProps {
+    trigger : ReactNode
+}
+
+const Drawers = ({trigger, children}: PropsWithChildren<DrawerProps>) => {
+    return (
+        <Drawer>
+            <DrawerTrigger asChild>
+                {trigger}
+            </DrawerTrigger>
+            <DrawerContent className="fixed bottom-4 inset-x-0 mx-auto max-w-[360px] w-full [&>div:first-child]:hidden rounded-t-030 !rounded-t-030 overflow-visible">
+                <div className="pl-024 pr-024">
+                    <DrawerHeader className="w-full flex items-end p-0">
+                        <DrawerTitle></DrawerTitle>
+                        <DrawerClose asChild>
+                            <button className="p-012 pr-0 mt-[8px] cursor-pointer">
+                                <CloseIcon/>
+                            </button>
+                        </DrawerClose>
+                    </DrawerHeader>
+                    {children}
+                </div>
+            </DrawerContent>
+        </Drawer>
+    );
+};
+
+export default Drawers;
