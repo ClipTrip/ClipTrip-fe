8a867b59ec04528a846a4f6df552b8d2bacfcf85
choikyungsoo <ryan1954@naver.com>
2025-07-28 14:39:41 +0900

    refactor: SelectField props 변경 및 내부 구조 변경

--- src/components/common/SelectField.tsx
+++ src/components/common/SelectField.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react"
+import {useState} from "react"
 import {
     Command,
     CommandGroup,
@@ -7,7 +7,6 @@ import {
 } from "@/components/ui/command"
 import {
     Popover,
-    PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { cn } from "@/lib/utils";
@@ -22,55 +21,56 @@ interface DataOption {
 interface SelectFieldProps {
     datas: DataOption[];
     placeHolder?: string;
-    setSelectedValue: (value: string) => void;
+    onChange?: (val: string) => void;
 }
 
-const SelectField = ({datas, placeHolder, setSelectedValue} : SelectFieldProps) => {
+const SelectField = ({datas, placeHolder, onChange} : SelectFieldProps) => {
     const [open, setOpen] = useState(false)
     const [value, setValue] = useState("")
 
-    useEffect(() => {
-        if(value){
-            setSelectedValue(value);
-        }
-    }, [value]);
-
     return (
-        <div className="pl-024 pr-024">
+        <div className="pl-024 pr-024 w-full">
             <Popover open={open} onOpenChange={setOpen}>
                 <PopoverTrigger asChild>
                     <button
                         type="button"
-                        className={cn("relative z-20 p-020 pt-016 pb-016 flex w-full justify-between bg-sy_container-neutral-normal rounded-020 body_m-prominent", open ? "border border-sy_line-super" : "border-none")}
+                        className={cn(
+                            "relative z-10 p-020 pt-016 pb-016 flex w-full justify-between bg-sy_container-neutral-normal rounded-020 body_m-prominent",
+                            open ? "border border-sy_line-super" : "border-none"
+                        )}
                     >
                         {value
                             ? datas?.find((data) => data.value === value)?.label
                             : placeHolder}
-                        {open ? <ArrowDropUpIcon /> : <ArrowDropDownIcon/>}
+                        {open ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
                     </button>
                 </PopoverTrigger>
-                <PopoverContent asChild className="w-full min-w-[312px] z-10 bg-sy_container-neutral-normal p-0 shadow-none border-none body_m-prominent">
-                    <Command className="bg-sy_container-neutral-normal mt-[-20px]">
-                        <CommandList className="pt-020">
-                            <CommandGroup className="text-sy_label-alternative rounded-010">
-                                {datas?.map((data) => (
-                                    <CommandItem
-                                        key={data.value}
-                                        value={data.value}
-                                        className="p-012"
-                                        onSelect={(currentValue) => {
-                                            setValue(currentValue === value ? "" : currentValue)
-                                            setOpen(false)
-                                        }}
-                                    >
-                                        {data.label}
-                                    </CommandItem>
-                                ))}
-                            </CommandGroup>
-                        </CommandList>
-                    </Command>
-                </PopoverContent>
+                {open && (
+                    <div className="w-full mt-2 bg-sy_container-neutral-normal rounded-020 z-0 mt-[-20px] ">
+                        <Command className="bg-sy_container-neutral-normal ">
+                            <CommandList className="pt-020">
+                                <CommandGroup className="text-sy_label-alternative">
+                                    {datas?.map((data) => (
+                                        <CommandItem
+                                            key={data.value}
+                                            value={data.value}
+                                            className="p-012 cursor-pointer hover:bg-sy_container-hover"
+                                            onSelect={(currentValue) => {
+                                                if(onChange) onChange(currentValue);
+                                                setValue(currentValue === value ? "" : currentValue);
+                                                setOpen(false);
+                                            }}
+                                        >
+                                            {data.label}
+                                        </CommandItem>
+                                    ))}
+                                </CommandGroup>
+                            </CommandList>
+                        </Command>
+                    </div>
+                )}
             </Popover>
+
         </div>
     );
 };
