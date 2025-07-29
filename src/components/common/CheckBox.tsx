a9bb03f82c5a72d75f9e89651ed9d6ed501bc367
choikyungsoo <ryan1954@naver.com>
2025-07-29 18:01:11 +0900

    refactor: 파일 구조에 맞게 CheckBox 구조 변경

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -3,34 +3,43 @@ import CheckCircleIcon from "@/components/icons/system/CheckCircleIcon.tsx";
 import ChevronRightIcon from "@/components/icons/system/ChevronRightIcon.tsx";
 
 interface CheckBoxProps {
+    id: string,
     label: string,
     type?: "default" | "primary",
     isChecked?: boolean,
-    setIsChecked: (value: boolean) => void,
+    setIsChecked?: (value: boolean) => void,
+    onClickRightIcon?: () => void,
 }
 
-const CheckBox = ({label, type = "default", isChecked, setIsChecked, ...props}: CheckBoxProps) => {
-
+const CheckBox = ({
+                      id,
+                      label,
+                      type = "default",
+                      isChecked,
+                      setIsChecked,
+                      onClickRightIcon,
+}: CheckBoxProps) => {
     return (
         <div className="pb-016 pt-016 flex justify-between items-center">
             <div>
                 <input
                     type="checkbox"
-                    id="checkbox"
+                    id={id}
                     className="hidden"
                     onChange={(e) => {
-                        setIsChecked(e.target.checked);
+                        if (setIsChecked) {
+                            setIsChecked(e.target.checked);
+                        }
                     }}
-                    {...props}
                 />
-                <label htmlFor="checkbox" className="flex items-center gap-016">
+                <label htmlFor={id} className="flex items-center gap-016">
                     <CheckCircleIcon isActive={isChecked}/>
                     <p className={cn(
                         type === "default" && "title_s text-sy_label-normal font-regular",
                         type === "primary" && "title_m-prominent font-bold")}>{label}</p>
                 </label>
             </div>
-            {type === "default" && <ChevronRightIcon/>}
+            {type === "default" && <button onClick={onClickRightIcon}><ChevronRightIcon/></button>}
         </div>
 
     );
