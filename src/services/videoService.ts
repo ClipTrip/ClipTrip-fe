bd30eb67b0e0cc5f3c0750c0679a544a3ab758d3
unknown <jjjk0605@naver.com>
2025-08-03 06:02:32 +0900

    fix: youtube api 호출 함수에서 header 삭제

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -3,12 +3,7 @@ import type { VideosRequest } from '@/types/video';
 
 export const videoApi = {
   youtube: async (data: VideosRequest) => {
-    const res = await instance.post('/api/v1/videos', data, {
-      headers: {
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqamprMDYwNUBuYXZlci5jb20iLCJhdXRoIjoiUk9MRV9VU0VSIiwidHlwZSI6IkFjY2VzcyIsImV4cCI6MTc1NDAzODU3OX0.EJA3cxfNpPrKdm1cTRM20p2uirOKgvnxpmDYx78KOvQ',
-      },
-    });
+    const res = await instance.post('/api/v1/videos', data);
 
     return res.data;
   },
