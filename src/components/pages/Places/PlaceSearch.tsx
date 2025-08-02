42e49e5be1b7af8489eb516e137141da46727047
unknown <jjjk0605@naver.com>
2025-08-03 06:10:57 +0900

    feat: 북마크에서 사용할 PlaceSearch 컴포넌트 구현

---
+++ src/components/pages/Places/PlaceSearch.tsx
@@ -0,0 +1,45 @@
+import ButtonIcon from '@/components/common/ButtonIcon';
+import SearchIcon from '@/components/icons/system/SearchIcon';
+import VideosLoading from '@/components/pages/Home/VideosLoading';
+import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface PlaceSearchProps {
+  onSearch?: (url: string) => void;
+  isPending?: boolean;
+}
+
+const PlaceSearch = ({ onSearch, isPending }: PlaceSearchProps) => {
+  const { t } = useTranslation('searchField');
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (searchRef.current?.value) onSearch?.(searchRef.current?.value);
+  };
+
+  return (
+    <>
+      {isPending && <VideosLoading />}
+      <form
+        onSubmit={handleSubmit}
+        className='bg-sy_container-neutral-white rounded-010 relative h-fit w-[312px]'
+      >
+        <input
+          placeholder={t('searchField')}
+          data-slot='search'
+          className='body_l placeholder:label_m placeholder:text-sy_label-light px-020 py-016 rounded-020 text-sy_label-normal h-14 w-full pr-12 outline-none'
+          ref={searchRef}
+        />
+
+        <ButtonIcon
+          className='[&>svg]:text-sy_icon-neutral-light absolute right-5 top-4 h-6 w-6'
+          Icon={SearchIcon}
+          disabled={isPending}
+        />
+      </form>
+    </>
+  );
+};
+
+export default PlaceSearch;
