f9c409216a70513254b4df5646343752369263b4
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2025-08-02 18:04:01 +0900

    [Feature/issue-027] 회원가입 및 이용약관 페이지 개발 #27 (#34)
    * fix: package.json 중복 코드 삭제
    
    * feat: login, Onboarding 페이지에 회원가입 페이지로 이동하도록 경로 추가
    
    * refactor: SelectField props 변경 및 내부 구조 변경
    
    * feat: CheckBox type이 Default일때 오른쪽 아이콘이 보이도록 추가
    
    * feat: Terms.tsx 이용약관 관련 페이지 완성
    
    * feat: SelectBox용 Hook 생성
    
    * feat: RegisterPage.tsx 페이지 완성
    
    * refactor: 파일 구조에 맞게 CheckBox 구조 변경
    
    * refactor: Terms 뒤로가기 네비게이션 방법 변경
    
    * refactor: useTranslated 훅삭제 및 DetailInfo 컴포넌트 리팩토링
    
    * refactor: Terms.tsx 삭제 및 컴포넌트 화
    
    * refactor: BasicInfo 코드 리뷰에 따른 버튼 클릭 함수 분리
    
    * refactor: DetailInfo에서 필요없는 타입 삭제
    
    * refactor: userStore 삭제 및 RegisterPage 리팩토링
    
    * refactor: SelectField 다른 요소위에 겹치도록 수정
    
    * fix: appbar 뒤로가기 처리 수정 및 회원가입 입력이 다되야 약관동의 할수 있도록 수정

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -1,43 +1,55 @@
-import { cn } from '@/lib/utils';
-import CheckCircleIcon from '@/components/icons/system/CheckCircleIcon.tsx';
+import { cn } from "@/lib/utils";
+import CheckCircleIcon from "@/components/icons/system/CheckCircleIcon.tsx";
+import ChevronRightIcon from "@/components/icons/system/ChevronRightIcon.tsx";
 
 interface CheckBoxProps {
+  id: string;
   label: string;
-  type?: 'default' | 'primary';
+  type?: "default" | "primary";
   isChecked?: boolean;
-  setIsChecked: (value: boolean) => void;
+  setIsChecked?: (value: boolean) => void;
+  onClickRightIcon?: () => void;
 }
 
 const CheckBox = ({
+  id,
   label,
-  type = 'default',
+  type = "default",
   isChecked,
   setIsChecked,
+  onClickRightIcon,
 }: CheckBoxProps) => {
   return (
-    <div className='p-012 pl-024 pr-024'>
-      <input
-        type='checkbox'
-        id='checkbox'
-        className='hidden'
-        onChange={(e) => {
-          setIsChecked(e.target.checked);
-        }}
-      />
-      <label
-        htmlFor='checkbox'
-        className='gap-016 flex items-center'
-      >
-        <CheckCircleIcon isActive={isChecked} />
-        <p
-          className={cn(
-            type === 'default' && 'title_s text-sy_label-normal font-regular',
-            type === 'primary' && 'title_m-prominent font-bold'
-          )}
-        >
-          {label}
-        </p>
-      </label>
+    <div className="pb-016 pt-016 flex justify-between items-center">
+      <div>
+        <input
+          type="checkbox"
+          id={id}
+          className="hidden"
+          checked={isChecked}
+          onChange={(e) => {
+            if (setIsChecked) {
+              setIsChecked(e.target.checked);
+            }
+          }}
+        />
+        <label htmlFor={id} className="flex items-center gap-016">
+          <CheckCircleIcon isActive={isChecked} />
+          <p
+            className={cn(
+              type === "default" && "title_s text-sy_label-normal font-regular",
+              type === "primary" && "title_m-prominent font-bold"
+            )}
+          >
+            {label}
+          </p>
+        </label>
+      </div>
+      {type === "default" && (
+        <button onClick={onClickRightIcon}>
+          <ChevronRightIcon />
+        </button>
+      )}
     </div>
   );
 };
