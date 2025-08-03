cf760fa2309220f4840aeef391608750a0fc8335
unknown <jjjk0605@naver.com>
2025-08-04 05:01:52 +0900

    feat: TextField 컴포넌트를 디자인 수정 사항에 맞게 디자인 수정 및 check 기능 추가

--- src/components/common/TextField.tsx
+++ src/components/common/TextField.tsx
@@ -8,6 +8,8 @@ interface TextFieldProps {
   onIconClick?: () => void;
   Icon?: ElementType;
   isError?: boolean;
+  checkText?: string;
+  onCheckClick?: () => void;
 }
 
 const TextField = ({
@@ -17,22 +19,31 @@ const TextField = ({
   isError,
   className,
   type,
+  checkText,
+  onCheckClick,
   ...props
 }: TextFieldProps & React.ComponentProps<'input'>) => {
   const [focused, setFocused] = useState(false);
 
   return (
-    <div className='bg-sy_container-neutral-normal h-fit w-[312px]'>
+    <div
+      className={cn(
+        'bg-sy_container-neutral-normal rounded-010 h-fit w-[312px]',
+        className
+      )}
+    >
       <div className='relative'>
         <input
           type={type}
           data-slot='input'
           className={cn(
-            'placeholder:label_m placeholder:text-sy_label-light p-012 rounded-010 text-sy_label-normal label_m h-12 w-full pr-12 outline-none',
+            'placeholder:label_m placeholder:text-sy_label-light p-012 rounded-010 text-sy_label-normal label_m h-14 w-full outline-none',
             'focus:border-sy_line-super focus:border',
             isError
               && 'border-sy_status-negative-normal focus:border-sy_status-negative-normal border',
-            className
+            checkText && 'pr-[70px]',
+            focused && 'pr-12',
+            checkText && focused && 'pr-[100px]'
           )}
           onFocus={() => setFocused(true)}
           onBlur={() => setFocused(false)}
@@ -41,10 +52,21 @@ const TextField = ({
         {focused && (
           <ButtonIcon
             type='button'
-            className='absolute right-0 top-0 bg-transparent active:bg-transparent'
+            className='absolute right-0 top-1/2 -translate-y-1/2 bg-transparent active:bg-transparent'
             Icon={Icon}
             onClick={onIconClick}
           />
+        )}
+        {checkText && (
+          <button
+            onClick={onCheckClick}
+            className={cn(
+              'label_m text-sy_label-light absolute right-5 top-1/2 -translate-y-1/2 cursor-pointer',
+              focused && 'right-12'
+            )}
+          >
+            {checkText}
+          </button>
         )}
       </div>
       {supportingText && (
