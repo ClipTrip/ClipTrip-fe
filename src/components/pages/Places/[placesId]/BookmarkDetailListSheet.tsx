02bdfcacdbcd9fa9fc2c22b8a3fd31992e6a5419
unknown <jjjk0605@naver.com>
2025-08-06 22:44:22 +0900

    feat: 북마크 이름 수정 기능 구현

--- src/components/pages/Places/[placesId]/BookmarkDetailListSheet.tsx
+++ src/components/pages/Places/[placesId]/BookmarkDetailListSheet.tsx
@@ -69,6 +69,7 @@ const BookmarkDetailListSheet = () => {
           defaultName={defaultName}
           open={open}
           onOpenChange={setOpen}
+          bookmarkId={bookmarkId}
         />
       )}
       <Sheet
