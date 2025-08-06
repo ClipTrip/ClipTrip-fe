fe2320343f44c70c95cd263b6aaea50d0eebc9fc
unknown <jjjk0605@naver.com>
2025-08-06 21:13:40 +0900

    chore: 백엔드 proxy 주소 변경

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'https://clip-trip.shop',
+        target: 'https://api.clip-trip.shop',
         changeOrigin: true,
         secure: true,
       },
