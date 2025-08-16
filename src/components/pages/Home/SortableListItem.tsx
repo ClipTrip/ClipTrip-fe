b8f7a8b611ab80cf3a017d7de4ecb846d87d8cb6
unknown <jjjk0605@naver.com>
2025-08-16 15:19:18 +0900

    fix: 드래그를 통해 위치 바꾸는 작업이 모바일에서는 제대로 수행되지 않던 오류 수정

--- src/components/pages/Home/SortableListItem.tsx
+++ src/components/pages/Home/SortableListItem.tsx
@@ -50,7 +50,7 @@ const SortableListItem = ({
             <button
               {...attributes}
               {...listeners}
-              className='cursor-pointer'
+              className='cursor-pointer touch-none'
             >
               <DragIcon />
             </button>
