3b4d91c30316e2448110a0f40741adb6a067073e
unknown <jjjk0605@naver.com>
2025-08-16 15:18:46 +0900

    refactor: ListItem관련 스타일 수정

--- src/components/common/ListItem_v2.tsx
+++ src/components/common/ListItem_v2.tsx
@@ -7,8 +7,8 @@ interface ListItem_v2Props {
 
 const ListItem_v2 = ({ text, Icon }: ListItem_v2Props) => {
   return (
-    <div className='bg-sy_container-neutral-white gap-008 flex h-11 w-[360px] items-center px-6'>
-      {Icon && <Icon className='size-6' />}
+    <div className='bg-sy_container-neutral-white gap-008 flex min-h-11 w-[360px] items-center px-6'>
+      {Icon && <Icon className='size-6 shrink-0' />}
       <span className='body_m-prominent text-sy_label-normal'>{text}</span>
     </div>
   );
