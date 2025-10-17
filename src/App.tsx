6864e5ca7f7b775aa2d279a5e09e55989d41865f
unknown <jjjk0605@naver.com>
2025-10-17 13:28:29 +0900

    feat: 번역 hook을 App에서 실행하여 서비스 전체에서 번역이 될 수 있도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
 import router from '@/routes/routes';
+import useGoogleTranslate from '@/hooks/useTranslate';
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  useGoogleTranslate();
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+      <div
+        id='google_translate_element'
+        style={{ display: 'none' }}
+      ></div>
+    </>
+  );
 }
 
 export default App;
