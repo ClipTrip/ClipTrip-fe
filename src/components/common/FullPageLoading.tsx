0993cdb8b61bbe2ead7a880c05954c8fb097e847
unknown <jjjk0605@naver.com>
2025-10-19 14:45:51 +0900

    refactor: FullPageLoading컴포넌트가 children을 가질 수 있도록 변경

--- src/components/common/FullPageLoading.tsx
+++ src/components/common/FullPageLoading.tsx
@@ -1,20 +1,23 @@
 import { createPortal } from 'react-dom';
 import Loading from '@/components/common/Loading';
 import { cn } from '@/lib/utils';
+import type { ReactNode } from 'react';
 
 interface FullPageLoadingProps {
   className?: string;
+  children?: ReactNode;
 }
 
-const FullPageLoading = ({ className }: FullPageLoadingProps) => {
+const FullPageLoading = ({ className, children }: FullPageLoadingProps) => {
   return createPortal(
     <div
       className={cn(
-        'fixed inset-0 z-[9999] flex items-center justify-center bg-black/60',
+        'fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-black/60',
         className
       )}
     >
       <Loading />
+      {children}
     </div>,
     document.body
   );
